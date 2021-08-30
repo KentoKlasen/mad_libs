15de664264789cd3f8d18c76bf77fce9e8404916
Kento Klasen <kento.klasen@gmail.com>
2021-08-29 20:03:32 -0700

    madLibs

---
+++ mad_libs.py
@@ -0,0 +1,53 @@
+def main():
+	"""This is a mad libs gernater"""
+	noun = input("choose a noun: ")
+	plural_noun = input("choose a plural noun: ")
+	noun_II = input("choose a second noun: ")
+	place = input("choose a place: ")
+	verb = input("choose a verb: ")
+	verb_II = input("choose a second verb: ")
+	#now we are making story
+	print("----------------------------")
+	print(f"I heard you were in love with a {noun}.")
+	print(f"Well i saw a {noun} over at the park.")
+	print(f"im hiding in your {place}.")
+	print(f"You should {verb} before i find you.")
+	print(f"you can {verb} but you cant {verb_II}.")
+	print(f"i am going to hunt you down with {plural_noun}...")
+	print("----------------------------")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+	main()
